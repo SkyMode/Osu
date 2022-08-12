6decb4d1d0e4ec328efff5e66037c2c3af8d60c8
SkyMode <76694859+SkyMode@users.noreply.github.com>
2022-08-12 18:04:56 +0800

    Add files via upload

---
+++ osu!/test.js
@@ -0,0 +1,15 @@
+//add const and let to the code
+const name = "John";
+let age = 25;
+
+
+//add function timer
+function timer() {
+  setTimeout(function() {
+    alert("I am an alert box!!");
+  }, 1000);
+}
+
+console.log(timer());
+
+
